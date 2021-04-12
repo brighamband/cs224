fac9f978546f277e28d435766c913834f7485f1c
Brigham Andersen <brighamband@gmail.com>
2021-04-12 09:52:25 -0600

    Added lab 4 and 5

---
+++ Lab5/target27/asm2.d
@@ -0,0 +1,9 @@
+
+2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 73 23 32 42 	mov    $0x42322373,%rdi
+   7:	c3                   	retq   
